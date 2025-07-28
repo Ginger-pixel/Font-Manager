30f28718797f39a756757b219859ab812ced490d
hyobinnSeo <hyobinn94@gmail.com>
2025-07-29 04:00:23 +0900

    더미섹션

--- index.js
+++ index.js
@@ -235,7 +235,6 @@ async function openFontManagementPopup() {
     
     // 모든 영역 렌더링
     renderPresetDropdown(template);
-    renderToggleSection(template); // 활성화 토글 렌더링
     renderUIFontSection(template);
     renderMessageFontSection(template);
     renderThemeLinkingSection(template);
@@ -298,12 +297,6 @@ function renderPresetDropdown(template) {
             dropdown.append(`<option value="${preset.id}" ${isSelected ? 'selected' : ''}>${preset.name}</option>`);
         });
     }
-}
-
-// 활성화 토글 렌더링
-function renderToggleSection(template) {
-    const toggle = template.find('#font-manager-enabled-toggle');
-    toggle.prop('checked', settings.enabled);
 }
 
 // UI 폰트 섹션 렌더링
@@ -617,12 +610,6 @@ function updateUIFont() {
         document.head.appendChild(fontStyle);
     }
     
-    // 폰트 매니저가 비활성화되어 있으면 스타일을 비움
-    if (!settings.enabled) {
-        fontStyle.innerHTML = '';
-        return;
-    }
-    
     const fontCss = [];
     const uiFontCss = [];
     const cssVariables = [];
@@ -898,13 +885,6 @@ function applyTempChatLineHeight(height) {
 
 // 이벤트 리스너 설정
 function setupEventListeners(template) {
-    // 활성화 토글 이벤트
-    template.find('#font-manager-enabled-toggle').off('change').on('change', function() {
-        settings.enabled = $(this).prop('checked');
-        updateUIFont();
-        saveSettingsDebounced();
-    });
-    
     // 프리셋 드롭다운 변경 이벤트
     template.find('#preset-dropdown').off('change').on('change', function() {
         const presetId = $(this).val();
